d258628178b505a8efe885c12cb02544c184915a
Kajune <ryuzoji21@gmail.com>
2021-01-27 02:48:04 +0000

    update

--- docker/build_server.sh
+++ docker/build_server.sh
@@ -4,3 +4,4 @@ docker build -t artwork_db docker/artwork_db
 docker run -d --name artwork_db artwork_db --default-authentication-plugin=mysql_native_password
 docker run -d -p 8080:80 -v $PWD/src:/var/www/html --name artwork_server --link artwork_db:artwork_db artwork_server
 chmod 777 $PWD/src/manage/tmp
+

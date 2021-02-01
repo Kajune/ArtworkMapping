a10be189676e1002b9930302bf4b2a62c31f0638
Kajune <ryuzoji21@gmail.com>
2021-02-01 20:33:28 +0900

    mysql backup setting

--- docker/build_server.bat
+++ docker/build_server.bat
@@ -1,5 +1,6 @@
 docker build -t artwork_server docker/artwork_server
 docker build -t artwork_db docker/artwork_db
 
-docker run -d --name artwork_db artwork_db --default-authentication-plugin=mysql_native_password
+docker volume create artwork_db_volume
+docker run -d -v artwork_db_volume:/var/lib/mysql --name artwork_db artwork_db --default-authentication-plugin=mysql_native_password
 docker run -d -p 8080:80 -v %CD%/src:/var/www/html --name artwork_server --link artwork_db:artwork_db artwork_server
--- docker/build_server.sh
+++ docker/build_server.sh
@@ -1,7 +1,8 @@
 docker build -t artwork_server docker/artwork_server
 docker build -t artwork_db docker/artwork_db
 
-docker run -d --name artwork_db artwork_db --default-authentication-plugin=mysql_native_password
+docker volume create artwork_db_volume
+docker run -d -v artwork_db_volume:/var/lib/mysql --name artwork_db artwork_db --default-authentication-plugin=mysql_native_password
 docker run -d -p 8080:80 -v $PWD/src:/var/www/html --name artwork_server --link artwork_db:artwork_db artwork_server
 chmod 777 $PWD/src/manage/tmp
 
